5cf6fdaa72ba711ed8a5555e3aa5c52262109059
Bitcoren <bitcoren@gmail.com>
2022-06-30 17:46:19 +0800

    Add puthon and nodejs to installation

--- install.cmd
+++ install.cmd
@@ -8,8 +8,8 @@ powershell -Command "Add-Content -Path .\path.ps1 -Value %value%"
 powershell -Command "Add-Content -Path .\path.ps1 -Value %MURZILLA%"
 powershell -Command "Add-Content -Path .\path.ps1 -Value (Get-Content '.\path.ps')"
 powershell -Command "Start-Process powershell -Verb RunAs -ArgumentList '& powershell %CD%\path.ps1'"
-.\bin\curl -L -o temp\ipfs.zip https://github.com/ipfs/go-ipfs/releases/download/v0.13.0/go-ipfs_v0.13.0_windows-amd64.zip
-.\bin\7z.exe x -otemp temp\ipfs.zip
+bin\curl -L -o temp\ipfs.zip https://github.com/ipfs/go-ipfs/releases/download/v0.13.0/go-ipfs_v0.13.0_windows-amd64.zip
+bin\7z.exe x -otemp temp\ipfs.zip
 move temp\go-ipfs\ipfs.exe bin\ipfs.exe
 del .\path.ps1
 bin\ipfs.exe init --profile server
@@ -24,8 +24,12 @@ find "ID" < temp\t.txt >> murzilla.log
 bin\ipfs.exe name publish QmQPeNsJPyVWPFDVHb77w8G42Fvo15z4bG2X8D2GhfbSXc >> murzilla.log
 bin\ipfs.exe get /ipfs/bafybeib4bqgd3t76g26tixd2j7ahugsedcjv6md73ipdauc5wjnzhh453y/emerblock.zip
 move emerblock.zip temp\emerblock.zip
-.\bin\7z.exe x -otemp temp\emerblock.zip
+bin\7z.exe x -otemp temp\emerblock.zip
 move temp\.emercoin data\emercoin
+bin\curl -L -o temp\python.exe https://www.python.org/ftp/python/3.10.5/python-3.10.5-amd64.exe
+temp\python.exe /quiet InstallAllUsers=1 PrependPath=1 Include_test=0
+bin\curl -L -o temp\node.msi https://nodejs.org/dist/v16.15.1/node-v16.15.1-x64.msi
+temp\node.msi
 rd /s /q temp
 mkdir temp
 timeout 9
