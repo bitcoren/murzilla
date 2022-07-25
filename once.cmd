6dda5452fbc0be0444fa52aaed0da5cc958bafda
Bitcoren <bitcoren@gmail.com>
2022-07-26 05:26:07 +0800

    Change npm update

--- once.cmd
+++ once.cmd
@@ -1,3 +1,3 @@
 python.exe -m pip install --upgrade pip
-npm install --location=global npm
+powershell -Command "npm install --location=global npm"
 del "%userprofile%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\once.cmd"
